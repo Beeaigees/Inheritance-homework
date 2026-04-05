a5db809c726dbc814734020cf74cf1bdaf151df2
Beeaigees <simpensiniajadeh242@gmail.com>
2026-04-05 14:30:26 +0700

    fix super not having ()

--- tugas/module_tugas/pajakPPN.py
+++ tugas/module_tugas/pajakPPN.py
@@ -5,3 +5,5 @@ class pajak_ppn(komponen_harga):
         super.__init__(subtotal)
         self.nilaipajak = self.subtotal * 0.1
         self.total_akhir += self.nilaipajak
+
+    def tambah_item(self,)
