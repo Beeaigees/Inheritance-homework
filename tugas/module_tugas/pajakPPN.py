8a006f775215c2dbc269237974dc580396fcc983
Beeaigees <simpensiniajadeh242@gmail.com>
2026-04-05 14:32:04 +0700

    fix

--- tugas/module_tugas/pajakPPN.py
+++ tugas/module_tugas/pajakPPN.py
@@ -2,7 +2,7 @@ from module_tugas.komponenHarga import komponen_harga
 
 class pajak_ppn(komponen_harga):
     def __init__(self, subtotal):
-        super.__init__(subtotal)
+        super().__init__(subtotal)
         self.nilaipajak = self.subtotal * 0.1
         self.total_akhir += self.nilaipajak
 
